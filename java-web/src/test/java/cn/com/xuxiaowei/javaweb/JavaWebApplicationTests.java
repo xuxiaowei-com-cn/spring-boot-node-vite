8676520563914f233c9e22c8197922ccf8e61d1e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-12-07 20:10:59 +0800

    java-web

---
+++ java-web/src/test/java/cn/com/xuxiaowei/javaweb/JavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.javaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
