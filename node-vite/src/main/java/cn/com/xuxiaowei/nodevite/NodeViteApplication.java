d5c6c64621f18f14c4346e1e2ab3c1249247286f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-12-07 20:13:47 +0800

    node-vite

---
+++ node-vite/src/main/java/cn/com/xuxiaowei/nodevite/NodeViteApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.nodevite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeViteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeViteApplication.class, args);
+    }
+
+}
---
+++ node-vite/src/test/java/cn/com/xuxiaowei/nodevite/NodeViteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.nodevite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeViteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
