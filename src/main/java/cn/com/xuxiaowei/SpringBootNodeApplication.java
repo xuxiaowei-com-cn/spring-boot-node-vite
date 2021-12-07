9702e9cc95fb827a7a71f13e97d3b865595f4a48
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-12-07 20:06:47 +0800

    init

---
+++ src/main/java/cn/com/xuxiaowei/SpringBootNodeApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/com/xuxiaowei/SpringBootNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
